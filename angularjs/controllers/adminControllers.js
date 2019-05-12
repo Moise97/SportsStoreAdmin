5b765cfab6e7a9af209b3e71d10ee03691ffa3b4
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-12 12:58:38 +0300

    Implementing the Orders Feature

--- angularjs/controllers/adminControllers.js
+++ angularjs/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module("sportsStoreAdmin")
 .constant("authUrl", "http://localhost:5500/users/login")
+.constant("ordersUrl", "http://localhost:5500/orders")
 .controller("authCtrl", function($scope, $http, $location, authUrl){
     $scope.authenticate = function(user, pass){
         $http({
@@ -33,4 +34,33 @@ angular.module("sportsStoreAdmin")
         "angularjs/views/adminProducts.html" : "angularjs/views/adminOrders.html";
     };
     
-});
+})
+.controller("ordersCtrl", function($scope, $http, ordersUrl){
+
+    $http({
+        method: 'GET',
+        url: ordersUrl,
+        withCredentials: true
+    })
+    .then(response => {     // success
+        $scope.orders = response.data;
+    })
+    .catch(error => {       // failed
+        $scope.error = error;
+    });
+
+    $scope.selectedOrder;
+
+    $scope.selectOrder = function(order){
+        $scope.selectedOrder = order;
+    }
+
+    $scope.calcTotal  = function(order){
+        var total = 0;
+        for(let i=0; i < order.products.length; i++){
+            total += order.products[i].count * order.products[i].price; 
+        }
+        return total;
+    }
+
+})
